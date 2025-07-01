0d9df3f25fa6515b4f458d48b9d2a28d20412e9d
Saumya Singh <singhsaumyait034@gmail.com>
2025-07-01 19:34:22 +0530

    July Coding Started

---
+++ src/July/LengthLastWord.java
@@ -0,0 +1,14 @@
+package July;
+
+public class LengthLastWord {
+    public static void main(String[] args){
+        String str = "Hi My name is Saumya Singh and I am an Automation Test Engineer";
+        findLengthofLastWord(str);
+    }
+
+    private static void findLengthofLastWord(String str) {
+        String[] words = str.split("\\s+");
+        System.out.println(words[words.length-1].length());
+        
+    }
+}
