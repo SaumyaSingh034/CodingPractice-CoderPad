c781ed4649996e4f8721b8aac07fe46d31a35031
Saumya Singh <singhsaumyait034@gmail.com>
2025-07-03 23:42:19 +0530

    String Manipulation Questions

--- src/July/StringContainOnlyDigits.java
+++ src/July/StringContainOnlyDigits.java
@@ -1,4 +1,24 @@
 package July;
 
 public class StringContainOnlyDigits {
+    public static void main(String[] args){
+        String n1 = "12345";
+        String n2 = "Saumya8778";
+        String n3 = "outputCheck";
+        System.out.println(isOnlyDigits(n1));
+        System.out.println(isOnlyDigits(n2));
+        System.out.println(isOnlyDigits(n3));
+    }
+
+    private static boolean isOnlyDigits(String str) {
+        if(str == null || str.isEmpty()){
+            throw new IllegalArgumentException("String is either empty or null");
+        }
+        for(char c : str.toCharArray()){
+            if(!Character.isDigit(c))
+                return false;
+
+        }
+        return true;
+    }
 }
