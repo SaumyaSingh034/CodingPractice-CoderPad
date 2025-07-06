387bcf7058e8ec4e333c4144597ada8023a95e60
Saumya Singh <singhsaumyait034@gmail.com>
2025-07-06 16:15:09 +0530

    Longest Uniform Substrin

--- src/CodePardFinal/LongestUniformSubString.java
+++ src/CodePardFinal/LongestUniformSubString.java
@@ -1,12 +1,39 @@
 package CodePardFinal;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class LongestUniformSubString {
     public static void main(String[] args){
         String input = "abcabcabcdddderffdddhujklnooo";
         int[] result = longestUniformSubstringCheck(input);
+        int[] result1 = longestNonRepeatedStringCheck(input);
         System.out.println("Max Length : "+result[1]);
         System.out.println("Substring : "+input.substring(result[0], result[0]+result[1]));
+        System.out.println("********************************************");
+        System.out.println("Max Length : "+result1[1]);
+        System.out.println("Substring : "+input.substring(result1[0], result1[0]+result1[1]));
 
+    }
+
+    private static int[] longestNonRepeatedStringCheck(String input) {
+        Set<Character> uniqueSet = new LinkedHashSet<>();
+        int left = 0;
+        int start = 0;
+        int maxLength = 0;
+        for(int i=0;i<input.length();i++){
+            char c = input.charAt(i);
+            while(uniqueSet.contains(c)){
+                uniqueSet.remove(input.charAt(left));
+                left++;
+            }
+            uniqueSet.add(c);
+            if(i-left+1 > maxLength){
+                start = left;
+                maxLength = i-left+1;
+            }
+        }
+        return new int[]{start, maxLength};
     }
 
     private static int[] longestUniformSubstringCheck(String input) {
