cd309c82c8b854e5fbcf652a06604d690dd706e0
Saumya Singh <singhsaumyait034@gmail.com>
2025-07-07 11:58:45 +0530

    Constructor

---
+++ src/CodePardFinal/Project.java
@@ -0,0 +1,23 @@
+package CodePardFinal;
+
+public class Project {
+
+    public Project(){
+        //Deafult Constructor
+        System.out.println("Deafult Constructor");
+    }
+
+    int a;
+    int b;
+    public Project(int a, int b){
+        this.a = a;
+        this.b = b;
+        System.out.println(a+" --> "+b);
+    }
+
+    
+    public static void main(String[] args){
+        Project p = new Project();
+        Project p1 = new Project(5,8);
+    }
+}
