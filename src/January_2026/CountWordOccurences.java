f6ae75807ab7369c5da8aaa6aa7daa0a384ff6c9
Saumya Singh <singhsaumyait034@gmail.com>
2026-01-11 17:28:08 +0530

    Second Largest Element

--- src/January_2026/CountWordOccurences.java
+++ src/January_2026/CountWordOccurences.java
@@ -1,12 +1,15 @@
 package January_2026;
 
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CountWordOccurences {
     public static void main(String[] args){
         String words = "I am a QA and I love QA";
         countTheOccurenceOfWords(words);
+        System.out.println(countOfOccurence(words));
     }
 
     private static void countTheOccurenceOfWords(String words) {
@@ -18,5 +21,12 @@ public class CountWordOccurences {
         for(Map.Entry<String, Integer> data : dataMap.entrySet()){
             System.out.println(data.getKey()+" ---> "+data.getValue());
         }
+    }
+
+    private static LinkedHashMap<String, Long> countOfOccurence(String words){
+        return Arrays.stream(words.split(" "))
+                .collect(Collectors.groupingBy(
+                        word -> word, LinkedHashMap::new, Collectors.counting()
+                ));
     }
 }
