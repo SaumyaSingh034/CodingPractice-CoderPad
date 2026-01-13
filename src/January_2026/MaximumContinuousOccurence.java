27d91156aaa1d96a95f496276a8187fe7ba56ad0
Saumya Singh <singhsaumyait034@gmail.com>
2026-01-13 10:56:43 +0530

    Maximum Continuous Occurence

--- src/January_2026/MaximumContinuousOccurence.java
+++ src/January_2026/MaximumContinuousOccurence.java
@@ -1,10 +1,7 @@
 package January_2026;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 public class MaximumContinuousOccurence {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String str = "aaabbcdddde";
         findMaximumContinuousOccurence(str);
         findMaximumContinuousOccurence("a");
@@ -16,27 +13,27 @@ public class MaximumContinuousOccurence {
     }
 
     private static void findMaximumContinuousOccurence(String str) {
-       if(str == null || str.length() ==0 ){
-           System.out.println("Empty String");
-           return;
-       }
+        if (str == null || str.length() == 0) {
+            System.out.println("Empty String");
+            return;
+        }
 
-       int  currentCount = 1;
-       int maxCount = 1;
-       char maxChar = str.charAt(0);
+        int currentCount = 1;
+        int maxCount = 1;
+        char maxChar = str.charAt(0);
         str = str.toLowerCase();
-       for(int i=1;i<str.length();i++){
-           if(str.charAt(i) == str.charAt(i-1)){
-               currentCount++;
-           }else{
-               currentCount = 1;
-           }
-           if(currentCount > maxCount){
-               maxCount = currentCount;
-               maxChar = str.charAt(i);
-           }
-       }
-       System.out.println(maxChar +" ---> "+maxCount);
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                currentCount++;
+            } else {
+                currentCount = 1;
+            }
+            if (currentCount > maxCount) {
+                maxCount = currentCount;
+                maxChar = str.charAt(i);
+            }
+        }
+        System.out.println(maxChar + " ---> " + maxCount);
 
 
     }
