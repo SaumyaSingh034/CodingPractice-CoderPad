b9415a3a6e7fbe5ae8f0cd7e3c77cc9118b16bd2
Saumya Singh <singhsaumyait034@gmail.com>
2026-01-28 12:08:47 +0530

    Swap

--- src/Beginner/Swap.java
+++ src/Beginner/Swap.java
@@ -1,4 +1,17 @@
 package Beginner;
 
 public class Swap {
+    public static void main(String[] args){
+        int a = 8;
+        int b = 9;
+        swapNumbers(a, b);
+
+    }
+
+    private static void swapNumbers(int a, int b) {
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        System.out.println(a+" ----- "+b);
+    }
 }
