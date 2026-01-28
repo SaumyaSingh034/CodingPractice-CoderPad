0e6ea43f6630a42748b13ada6a426a977cde2d92
Saumya Singh <singhsaumyait034@gmail.com>
2026-01-28 12:07:24 +0530

    Prime Number Check

--- src/Beginner/Prime.java
+++ src/Beginner/Prime.java
@@ -1,4 +1,26 @@
 package Beginner;
 
 public class Prime {
+    public static void main(String[] args){
+        int number = 4771;
+        checkNumberisPrimeOrNot(number);
+    }
+
+    private static void checkNumberisPrimeOrNot(int number) {
+        if(number <=1){
+            System.out.println("NOT PRIME");
+        }
+        boolean isPrime = true;
+        for(int i=2;i<=Math.sqrt(number);i++){
+            if(number % i==0){
+                isPrime = false;
+                break;
+
+            }
+        }
+        if(isPrime)
+            System.out.println("PRIME");
+        else
+            System.out.println("NOT PRIME");
+    }
 }
