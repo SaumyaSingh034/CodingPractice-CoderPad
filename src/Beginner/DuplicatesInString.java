cb346c9f3d3499d7226a25b616e722fe9280087c
Saumya Singh <singhsaumyait034@gmail.com>
2026-01-28 11:55:50 +0530

    Duplicates removal for String

--- src/Beginner/DuplicatesInString.java
+++ src/Beginner/DuplicatesInString.java
@@ -1,4 +1,21 @@
 package Beginner;
 
+import July.SubArraySum;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class DuplicatesInString {
+    public static void main(String[] args){
+        String str = "duplicates removal for automation";
+        removeDuplicatesFromString(str.toCharArray());
+    }
+
+    private static void removeDuplicatesFromString(char[] charArray) {
+        Set<Character> uniqueChar = new LinkedHashSet<>();
+        for(char c : charArray){
+            uniqueChar.add(c);
+        }
+        System.out.println(uniqueChar);
+    }
 }
