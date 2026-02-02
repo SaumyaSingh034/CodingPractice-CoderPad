e2146b085eb2945fa5a10229cc6f0ae4c6533eeb
Saumya Singh <singhsaumyait034@gmail.com>
2026-02-02 15:43:42 +0530

    interface Implementation

---
+++ src/Fenruary_2026/Teams.java
@@ -0,0 +1,8 @@
+package Fenruary_2026;
+
+public class Teams implements TestInterface{
+    @Override
+    public void teams() {
+        System.out.println("This is Team India");
+    }
+}
---
+++ src/Fenruary_2026/TestInterface.java
@@ -0,0 +1,12 @@
+package Fenruary_2026;
+
+public interface TestInterface {
+
+     void teams();
+
+      static void sports(){
+         System.out.println("This is Cricket we are talking about");
+     }
+
+
+}
