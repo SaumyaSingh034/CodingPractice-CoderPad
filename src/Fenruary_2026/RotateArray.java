7b6b7d2232e6cf086209c33885533c3c532be2a1
Saumya Singh <singhsaumyait034@gmail.com>
2026-02-03 11:21:41 +0530

    Rotate Array

---
+++ src/Fenruary_2026/RotateArray.java
@@ -0,0 +1,15 @@
+package Fenruary_2026;
+
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5};
+        int last = arr[arr.length-1];
+      for(int i=arr.length-1;i>0;i--){
+          arr[i] = arr[i-1];
+      }
+      arr[0] = last;
+      System.out.println(Arrays.toString(arr));
+    }
+}
