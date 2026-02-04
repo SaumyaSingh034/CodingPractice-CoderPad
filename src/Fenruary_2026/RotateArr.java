8ebc8fd96d20d7953ceb5d0dbf4f34771fcd8232
Saumya Singh <singhsaumyait034@gmail.com>
2026-02-04 09:12:51 +0530

    test

--- src/Fenruary_2026/RotateArr.java
+++ src/Fenruary_2026/RotateArr.java
@@ -5,7 +5,24 @@ import java.util.Arrays;
 public class RotateArr {
     public static void main(String[] args){
         int[] arr = {1,2,3,4,5}; //5,1,2,3,4 //4,5,1,2,3 //3,4,5,1,2
-        int rotation = 3;
+        int rotation = 2;
+
+        //Rotate left
+
+        for(int i=0;i<rotation;i++){
+            int first = arr[0];
+            for(int j=0;j<arr.length-1;j++) {
+                arr[j] = arr[j+1];
+            }
+            arr[arr.length-1] = first;
+        }
+        System.out.println(Arrays.toString(arr));
+
+
+
+
+
+        //Rotate right
         for(int i=0;i<rotation;i++){
             int last = arr[arr.length-1];
             for(int j = arr.length-1;j>0;j--){
