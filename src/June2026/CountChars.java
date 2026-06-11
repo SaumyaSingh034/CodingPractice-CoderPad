cc5bab631069a2f683de41817191023826f2fc0c
Saumya Singh <singhsaumyait034@gmail.com>
2026-06-11 23:08:20 +0530

    June Coding Practice Started

--- src/June2026/CountChars.java
+++ src/June2026/CountChars.java
@@ -5,8 +5,21 @@ import java.util.Map;
 
 public class CountChars {
     public static void main(String[] args){
-        String str = "aabbbcccc";
+        String str = "aabbbccccddddd"; //a2b3c4d5
         countChars(str);
+        compressString_test(str);
+    }
+
+    private static void compressString_test(String str) {
+        Map<Character, Integer> dataSet = new LinkedHashMap<>();
+        for(char c : str.toLowerCase().toCharArray()){
+            dataSet.put(c, dataSet.getOrDefault(c, 0)+1);
+        }
+        StringBuilder result = new StringBuilder();
+        for(Map.Entry<Character, Integer> data : dataSet.entrySet()){
+           result.append(data.getKey()).append(data.getValue());
+        }
+        System.out.println(result.toString().trim());
     }
 
     private static void countChars(String str) {
