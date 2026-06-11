9f697094080131dd625f38ddf5312322da85b60b
Saumya Singh <singhsaumyait034@gmail.com>
2026-06-11 23:01:14 +0530

    June Coding Practice Started

---
+++ src/June2026/FirstChar.java
@@ -0,0 +1,25 @@
+package June2026;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FirstChar {
+    public static void main(String[] args){
+        String str = "automation";
+       System.out.println( findFirstNonRepeat(str));
+    }
+
+    private static Character findFirstNonRepeat(String str) {
+        Map<Character, Integer> hashMap = new LinkedHashMap<>();
+        for(char c : str.toLowerCase().toCharArray()){
+            hashMap.put(c, hashMap.getOrDefault(c,0)+1);
+        }
+
+
+        for(Map.Entry<Character, Integer> dataSet : hashMap.entrySet()){
+            if(dataSet.getValue() == 1)
+                return dataSet.getKey();
+        }
+        return '_';
+    }
+}
