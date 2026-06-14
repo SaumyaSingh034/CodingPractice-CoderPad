480ba7bc52a8b421709f88b0957f6e61b57d1a1e
Saumya Singh <singhsaumyait034@gmail.com>
2026-06-14 15:11:21 +0530

    June Coding Practice Started

---
+++ src/June2026/CustomException_Test.java
@@ -0,0 +1,8 @@
+package June2026;
+
+public class CustomException_Test extends Exception{
+
+    public  CustomException_Test(String msg){
+        super(msg);
+    }
+}
---
+++ src/June2026/InsufficentBalance.java
@@ -0,0 +1,8 @@
+package June2026;
+
+public class InsufficentBalance extends Exception{
+
+    public InsufficentBalance(String msg){
+        super(msg);
+    }
+}
