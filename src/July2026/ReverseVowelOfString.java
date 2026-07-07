c12b753028efdbc5db6612e063af04e413b8d8a0
Saumya Singh <singhsaumyait034@gmail.com>
2026-07-07 19:28:43 +0530

    Hard String Reverse

---
+++ src/July2026/ReverseVowelOfString.java
@@ -0,0 +1,37 @@
+package July2026;
+
+import java.util.Arrays;
+
+public class ReverseVowelOfString {
+    public static void main(String[] args){
+        String input = "hello";
+        reverseOnlyVowels(input);
+    }
+
+    private static void reverseOnlyVowels(String input) {
+        if(input == null || input.isEmpty()){
+            throw new IllegalArgumentException("Input String is empty");
+        }
+        int left = 0;
+        int right = input.length()-1;
+        char[] arr = input.toCharArray();
+        while(left < right){
+            if(isVowel(arr[left]) && isVowel(arr[right])){
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+
+            }
+            left++;
+            right--;
+        }
+        System.out.println(new String(arr));
+
+
+
+    }
+
+    private static boolean isVowel(char c) {
+        return c == 'a' || c =='e' || c=='i' || c=='o' || c=='u';
+    }
+}
