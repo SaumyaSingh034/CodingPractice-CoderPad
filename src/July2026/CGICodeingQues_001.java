bf2022a03fd6156092e47941a48db25ce65a51cb
Saumya Singh <singhsaumyait034@gmail.com>
2026-07-08 18:54:10 +0530

    Correct Code

--- src/July2026/CGICodeingQues_001.java
+++ src/July2026/CGICodeingQues_001.java
@@ -1,4 +1,28 @@
 package July2026;
 
 public class CGICodeingQues_001 {
+    public static void main(String[] args) {
+        /*Check if all ‘a’ come before ‘b’ in the string.
+                Input: “aaabb” → true
+                Input: “ababa” → false
+
+         */
+        String input = "aaabbaaaab";
+        String input1 = "aaabb";
+        String input2 = "ababa";
+        String input3 = "aaaa";
+        System.out.println(stringValidation(input));
+        System.out.println(stringValidation(input1));
+        System.out.println(stringValidation(input2));
+        System.out.println(stringValidation(input3));
+    }
+
+    private static boolean stringValidation(String input) {
+           for(int i=0;i<input.length()-1;i++){
+               if(input.charAt(i) == 'b' && input.charAt(i+1) == 'a'){
+                       return false;
+               }
+           }
+        return true;
+    }
 }
